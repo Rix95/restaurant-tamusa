e1b887817ac020e20c8e4036ff10ce388b991a9c
RixDota <ricalrax@gmail.com>
2023-07-23 18:15:31 -0500

    added cart and other css properties to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,41 @@
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}></div>
-      <div className={styles.callButton}>
-       <Image src=" 
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="/img/telephone.png"
+            alt="telephone"
+            width="32"
+            height="32"
+          />
+        </div>
+
+        <div className={styles.texts}>
+          <div className={styles.text}>Order Now</div>
+          <div className={styles.text}>(210) 800 1234</div>
+        </div>
       </div>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="logo" width="160" height="70" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="logo" width="160" height="70" />
+          <div classname={styles.counter}>999</div>
+        </div>
+      </div>
     </div>
   );
 };
