a89b7ef208b5509b49ae2d4ce6d938b9f31090a6
RixDota <ricalrax@gmail.com>
2023-07-24 00:02:36 -0500

    added footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className={styles.item}>
         <div className={styles.card}>
           <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+            OH YES, WE DID.THE LAMA PIZZA, <br></br>WELL BAKED SLICE OF PIZZA.
           </h2>
         </div>
         <div className={styles.card}>
---
+++ pages/product/[id].jsx
@@ -0,0 +1,8 @@
+import styles from "../../styles/Product.module.css";
+import Image from "next/image";
+
+const Product = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Product;
