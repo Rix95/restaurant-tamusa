f538a957e93220bdffdd832a35ee29c35868a344
RixDota <ricalrax@gmail.com>
2023-07-23 22:08:01 -0500

    added elements to productcard and list module

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import styles from "../styles/ProductCard.module.css";
+
 const ProductCard = () => {
-  return <div>borger</div>;
+  return (
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="300" height="300" />
+      <h1 className={styles.title}>Very Delicious Borgers</h1>
+      <span className={styles.price}> $19.90 </span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+      </p>
+    </div>
+  );
 };
 
 export default ProductCard;
