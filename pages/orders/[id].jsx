bd4d356cf615473843ddc26edaef095f01088511
e-kari <enya.kpg@gmail.com>
2023-07-25 20:30:52 -0500

    added 768px responsive breakpoint to order page

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -14,26 +14,28 @@ const Order = () => {
       <div className={styles.left}>
         <div className={styles.row}>
           <table className={styles.table}>
-            <tr className={styles.trTitle}>
-              <th>Order ID</th>
-              <th>Customer</th>
-              <th>Address</th>
-              <th>Total</th>
-            </tr>
-            <tr className={styles.tr}>
-              <td>
-                <span className={styles.id}>129837819237</span>
-              </td>
-              <td>
-                <span className={styles.name}>John Doe</span>
-              </td>
-              <td>
-                <span className={styles.address}>Elton st. 212-33 LA</span>
-              </td>
-              <td>
-                <span className={styles.total}>$79.80</span>
-              </td>
-            </tr>
+            <tbody>
+              <tr className={styles.trTitle}>
+                <th>Order ID</th>
+                <th>Customer</th>
+                <th>Address</th>
+                <th>Total</th>
+              </tr>
+              <tr className={styles.tr}>
+                <td>
+                  <span className={styles.id}>129837819237</span>
+                </td>
+                <td>
+                  <span className={styles.name}>John Doe</span>
+                </td>
+                <td>
+                  <span className={styles.address}>Elton st. 212-33 LA</span>
+                </td>
+                <td>
+                  <span className={styles.total}>$79.80</span>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className={styles.row}>
