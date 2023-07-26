4a02e16e443f794a811e136450bc4185a9fb1cd3
RixDota <ricalrax@gmail.com>
2023-07-25 22:09:44 -0500

    added packages ky and axios

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,12 +7,18 @@ export default async function handler(req, res) {
   dbConnect();
 
   if (method === "GET") {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 
   if (method === "POST") {
     try {
       const product = await Product.create(req.body);
-      res.status(201);
+      res.status(201).json(product);
     } catch (err) {
       res.status(500).json(err);
     }
