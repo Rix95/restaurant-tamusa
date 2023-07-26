bd7579375343a7230a2b2ab5c0dc8143a9316f4a
RixDota <ricalrax@gmail.com>
2023-07-25 19:06:43 -0500

    fixed cart added <tbody>

---
+++ models/Order.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxLength: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxLength: 200,
+    },
+
+    img: {
+      type: String,
+      required: true,
+    },
+
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    status: {
+      type: Number,
+      default: 0,
+    },
+    method: {
+      type: Number,
+      required: true,
+    },
+
+    extraOptions: {
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
