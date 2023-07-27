3d07299954a7790e40c2dad4682b2a1e8f8a77b0
ediaz029 <ediazj03@gmail.com>
2023-07-26 19:10:25 -0700

    new commit (ernesto)

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/ProductCard.module.css";
+import Link from "next/link";
 
 const ProductCard = ({ product }) => {
   return (
     <div className={styles.container}>
-      <Image src={product.img} alt="" width="300" height="300" />
+      <Link href={ `/product/${product._id}`} passHref>
+        <Image src={product.img} alt="" width="300" height="300" />
+      </Link>
       <h1 className={styles.title}>{product.title}</h1>
       <span className={styles.price}> ${product.prices[0]} </span>
       <p className={styles.desc}>{product.desc}</p>
