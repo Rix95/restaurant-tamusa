c308ae86a459231268d99354c1ee4562e2b7b377
ediaz029 <ediazj03@gmail.com>
2023-07-26 20:30:37 -0700

    fixed axios error

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,21 +2,24 @@ import styles from "../../styles/Product.module.css";
 import Image from "next/image";
 import { useState } from "react";
 import axios from "axios";
+import { Piazzolla } from "next/font/google";
 
 const Product = ({product}) => {
   const [size, setSize] = useState(0);
+
+  console.log(product)
 
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.imgContainer}>
-          <Image src={pizza.img} objectFit="contain" layout="fill" alt="" />
+          <Image src={product.img} objectFit="contain" layout="fill" alt="" />
         </div>
       </div>
       <div className={styles.right}>
-        <h1 className={styles.title}>{pizza.name}</h1>
-        <span className={styles.price}>${pizza.prices[size]}</span>
-        <p className={styles.desc}>{pizza.desc}</p>
+        <h1 className={styles.title}>{product.title}</h1>
+        <span className={styles.price}>${product.prices[size]}</span>
+        <p className={styles.desc}>{product.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
           <div className={styles.size} onClick={() => setSize(0)}>
@@ -34,42 +37,17 @@ const Product = ({product}) => {
         </div>
         <h3 className={styles.choose}>Choose additional ingredients</h3>
         <div className={styles.ingredients}>
-          <div className={styles.option}>
-            <input
-              type="checkbox"
-              id="double"
-              name="double"
-              className={styles.checkbox}
-            />
-            <label htmlFor="double">Double Ingredients</label>
-          </div>
-          <div className={styles.option}>
-            <input
-              className={styles.checkbox}
-              type="checkbox"
-              id="cheese"
-              name="cheese"
-            />
-            <label htmlFor="cheese">Extra Cheese</label>
-          </div>
-          <div className={styles.option}>
-            <input
-              className={styles.checkbox}
-              type="checkbox"
-              id="spicy"
-              name="spicy"
-            />
-            <label htmlFor="spicy">Spicy Sauce</label>
-          </div>
-          <div className={styles.option}>
-            <input
-              className={styles.checkbox}
-              type="checkbox"
-              id="garlic"
-              name="garlic"
-            />
-            <label htmlFor="garlic">Garlic Sauce</label>
-          </div>
+          {product.extraOptions.map(option=>(
+            <div className={styles.option} key={option._id}>
+              <input
+                type="checkbox"
+                id="double"
+                name="double"
+                className={styles.checkbox}
+              />
+              <label htmlFor="double">{option.text}</label>
+            </div>
+          ))}
         </div>
         <div className={styles.add}>
           <input type="number" defaultValue={1} className={styles.quantity} />
@@ -81,7 +59,7 @@ const Product = ({product}) => {
 };
 
 export const getServerSideProps = async ({params }) => {
-  const res = await axios(`http://localhost:3000/api/products/${params.id}`);
+  const res = await axios.get(`http://localhost:3000/api/products/${params.id}`);
   return {
     props: {
       product: res.data,
