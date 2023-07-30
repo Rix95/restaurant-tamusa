d11b471346790757aca4a6eaa9e585c8475fa125
ediaz029 <ediazj03@gmail.com>
2023-07-30 05:50:56 -0500

    cookie auth

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,10 +4,10 @@ const handler = (req, res) => {
   if (req.method === "POST") {
     const { username, password } = req.body;
     if (
-      username === process.env.USER_NAME &&
-      password === process.env.PASSWORD
+      username === process.env.ADMIN_USERNAME &&
+      password === process.env.ADMIN_PASSWORD
     ) {
-      res.setHeaders(
+      res.setHeader(
         "Set-Cookie",
         cookie.serialize("token", process.env.TOKEN, {
           maxAge: 60 * 60,
@@ -17,7 +17,7 @@ const handler = (req, res) => {
       );
       res.status(200).json("Success");
     } else {
-      res.status(400).json("Invalid Credentials");
+      res.status(400).json("Wrong Credentials!");
     }
   }
 };
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 import Head from "next/head";
+import { Inter, Space_Mono } from "next/font/google";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Featured from "@/components/Featured";
 import ProductList from "@/components/ProductList";
 import axios from "axios";
+import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ productList }) {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Restaurant Round 3</title>
         <meta name="description" content="Grade A Restaurant" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Featured />
+      <Featured/>
       <ProductList productList={productList} />
-    </>
+    </div>
   );
 }
 
