c68a9f1c958675bfed638e4559f76d8214c7d6fe
Rix95 <ricalrax@gmail.com>
2023-07-30 20:58:29 -0500

    fixed api products json get

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   if (method === "GET") {
     try {
       const products = await Product.find();
-      res.status(200).json();
+      res.status(200).json(products);
     } catch (err) {
       res.status(500).json(err);
     }
