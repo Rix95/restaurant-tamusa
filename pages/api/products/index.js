aa38009c561732caea3000efd522490463642039
RixDota <ricalrax@gmail.com>
2023-07-31 23:39:03 -0500

    fixed products, replaced logo/img

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   }
 
   if (method === "POST") {
-    if (!token || token !== process.env.token) {
+    if (!token || token !== process.env.TOKEN) {
       return res.status(401).json("Error: User not authorized.");
     }
     try {
